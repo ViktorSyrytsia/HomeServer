5e80d1eea46fa95a5b4b3f57f9cca1db1c16a1ba
Viktor Syrytsia <vsyrytsia@elinext.com>
2021-08-01 19:52:52 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { configService } from './config/config.service';
+import { ItemController } from './item/item.controller';
+import { ItemService } from './item/item.service';
+import { ItemService } from './item/item.service';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+  controllers: [AppController, ItemController],
+  providers: [AppService, ItemService],
+})
+export class AppModule {}
