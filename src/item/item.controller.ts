a2d91e5d6d8d3518961463c42072cf60596cbfeb
Viktor Syrytsia <vsyrytsia@elinext.com>
2021-08-01 20:13:06 +0300

    implemented base server structure

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ItemService } from './item.service';
 
 @Controller('item')
-export class ItemController {}
+export class ItemController {
+  constructor(private service: ItemService) {}
+
+  @Get()
+  public async getAll() {
+    return await this.service.getAll();
+  }
+
+  @Post()
+  public async createItem() {
+    return await this.service.createItem();
+  }
+}
--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from '../models/item.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ItemService {}
+export class ItemService {
+  constructor(
+    @InjectRepository(Item) private readonly repo: Repository<Item>,
+  ) {}
+
+  public async getAll(): Promise<Item[]> {
+    return await this.repo.find();
+  }
+
+  public async createItem(): Promise<Item> {
+    return  this.repo.save({
+      createDateTime: new Date(Date.now()),
+      createdBy: 'Admin',
+      description: 'Some short description',
+      internalComment: 'Internal comment test',
+      isActive: true,
+      isArchived: false,
+      lastChangedBy: 'Admin',
+      lastChangedDateTime: new Date(Date.now()),
+      name: 'First test item'
+    });
+  }
+}
